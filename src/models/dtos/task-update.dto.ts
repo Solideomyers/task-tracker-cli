e04b381d6461cefd0e36dfb796c672b5431c5be9
Francisco A. Myers M. <franciscomyers@gmail.com>
2024-09-04 11:06:18 -0400

    feat: dto update task

---
+++ src/models/dtos/task-update.dto.ts
@@ -0,0 +1,9 @@
+export class TaskUpdateDto {
+  public readonly name?: string;
+  public readonly description?: string;
+
+  constructor(name: string, description: string) {
+    this.name = name;
+    this.description = description;
+  }
+}
