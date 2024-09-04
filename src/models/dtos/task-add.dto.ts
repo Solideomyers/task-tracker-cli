dd51ddb4267f347c12c8f6a42cfd269578ee8782
Francisco A. Myers M. <franciscomyers@gmail.com>
2024-09-04 11:05:55 -0400

    feat: dto add task

---
+++ src/models/dtos/task-add.dto.ts
@@ -0,0 +1,9 @@
+export class TaskAddDto {
+  public readonly name: string;
+  public readonly description: string;
+
+  constructor(name: string, description: string) {
+    this.name = name;
+    this.description = description;
+  }
+}
