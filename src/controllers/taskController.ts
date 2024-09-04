a5cee8ad73c88345a7c482a212d8c837d278d744
Francisco A. Myers M. <franciscomyers@gmail.com>
2024-09-04 11:15:53 -0400

    feat: refactor and documantation

--- src/controllers/taskController.ts
+++ src/controllers/taskController.ts
@@ -1,33 +1,68 @@
+import { TaskError } from '../errors/task.errors';
+import { Status } from '../models/enums/status.enum';
 import { TaskService } from '../services/taskService';
 
+/**
+ * @class TaskController
+ * @description Controller to manage task-related commands
+ */
 export class TaskController {
-  static handleCommand(command: string, args: string[]): void {
-    switch (command) {
-      case 'add':
-        TaskService.addTask(args.join(' '));
-        break;
-      case 'update':
-        TaskService.updateTask(parseInt(args[0]), args.slice(1).join(' '));
-        break;
-      case 'delete':
-        TaskService.deleteTask(parseInt(args[0]));
-        break;
-      case 'mark-in-progress':
-        TaskService.markTaskInProgress(parseInt(args[0]));
-        break;
-      case 'mark-done':
-        TaskService.markTaskDone(parseInt(args[0]));
-        break;
-      case 'list':
-        if (args[0]) {
-          TaskService.listTasks(args[0] as 'todo' | 'in-progress' | 'done');
-        } else {
-          TaskService.listTasks();
-        }
-        break;
-      default:
-        console.log('Unknown command.');
-        break;
+  private taskService: TaskService;
+
+  /**
+   * @constructor
+   * Initializes the TaskServices instance
+   */
+  constructor() {
+    this.taskService = new TaskService();
+  }
+
+  /**
+   *
+   * @param command - The command to execute
+   * @param args - Argumentes related to the command
+   */
+  public handleCommand(command: string, args: string[]): void {
+    try {
+      switch (command) {
+        case 'add':
+          this.taskService.promptAddTask();
+          break;
+        case 'update':
+          this.taskService.promptUpdateTask();
+          break;
+        case 'delete':
+          this.taskService.deleteTask(parseInt(args[0]));
+          break;
+        case 'mark-in-progress':
+          this.taskService.markTaskInProgress(parseInt(args[0]));
+          break;
+        case 'mark-done':
+          this.taskService.markTaskDone(parseInt(args[0]));
+          break;
+        case 'list':
+          if (args[0]) {
+            this.taskService.listTasks(args[0] as Status);
+          } else {
+            this.taskService.listTasks();
+          }
+          break;
+        case 'find':
+          this.taskService.findTask(parseInt(args[0]));
+          break;
+        case 'delete-all':
+          this.taskService.deleteAllTasks();
+          break;
+        default:
+          console.log('Unknown command.');
+          break;
+      }
+    } catch (error) {
+      if (error instanceof TaskError) {
+        console.log(error.getFriendlyMessage());
+      } else {
+        console.error('An unexpected error occurred.');
+      }
     }
   }
 }
